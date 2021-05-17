28fbaabbffbe205d3f0067e065d0c0cc0cb11d32
daivt <daiproxomhoa@gmail.com>
2021-05-17 17:01:16 +0700

    17/5

--- src/test/BodyEnd/index.jsx
+++ src/test/BodyEnd/index.jsx
@@ -33,7 +33,7 @@ function BodyEnd(props) {
         </div>
         <div className="row" style={{ margin: "0 11px" }}>
           <div className="body-end-body-rate body-end-body-rate-left">
-            <div className="row">
+            <div className="row body-end-body-rate-left-row ">
               <div className="progress-group-text">Monday</div>
               <div className="progress-group-bars">
                 <div className="progress progress-xs">
@@ -57,9 +57,300 @@ function BodyEnd(props) {
                 </div>
               </div>
             </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Tuesday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "56%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "94%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Wednesday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "12%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "67%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Thursday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "43%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "91%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Friday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "22%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "73%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Saturday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "53%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "82%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="row body-end-body-rate-left-row">
+              <div className="progress-group-text">Sunday</div>
+              <div className="progress-group-bars">
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-info"
+                    role="progressbar"
+                    aria-valuenow="34"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "9%" }}
+                  ></div>
+                </div>
+                <div className="progress progress-xs">
+                  <div
+                    className="progress-bar bg-danger"
+                    role="progressbar"
+                    aria-valuenow="78"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "69%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div class="note text-center">
+              <small>
+                <sup class="px px-1">
+                  <span class="badge badge-info badge-pill">&nbsp;</span>
+                </sup>
+                New clients &nbsp;
+                <sup class="px px-2">
+                  <span class="badge badge-danger badge-pill">&nbsp;</span>
+                </sup>
+                Recurring clients
+              </small>
+            </div>
           </div>
           <div className="body-end-body-rate body-end-body-rate-right">
-            Male
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-male" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">Male</div>
+                <div className="body-end-body-rate-right-rate">43%</div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "43%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-female" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">
+                  Female
+                </div>
+                <div className="body-end-body-rate-right-rate">37%</div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "37%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-globe" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">
+                  Organic Search
+                </div>
+                <div className="body-end-body-rate-right-rate">
+                  191,235 (56%)
+                </div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning-3"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "56%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">
+                  Facebook
+                </div>
+                <div className="body-end-body-rate-right-rate">
+                  51,223 (15%)
+                </div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning-3"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "15%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">
+                  Twitter
+                </div>
+                <div className="body-end-body-rate-right-rate">
+                  37,564 (11%)
+                </div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning-3"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "11%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="body-end-body-rate-right-row">
+              <div className="row body-end-body-rate-right-row-1">
+                <div className="body-end-body-rate-right-icon">
+                  <i class="fa fa-male" aria-hidden="true"></i>
+                </div>
+                <div className="body-end-body-rate-right-information">
+                  LinkedIn
+                </div>
+                <div className="body-end-body-rate-right-rate">27,319 (8%)</div>
+              </div>
+              <div class="progress progress-xs">
+                <div
+                  class="progress-bar bg-warning-3"
+                  role="progressbar"
+                  aria-valuenow="43"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{ width: "8%" }}
+                ></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
